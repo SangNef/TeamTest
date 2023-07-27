6db5d4266bb274a92d9fceb2a0a84616b58aa2cb
huudao2004zz <nguyenhuudao3211@gmail.com>
2023-07-27 22:52:50 +0700

    create backend

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -28,6 +28,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    public $timestamps = false;
     protected $hidden = [
         'password',
         'remember_token',
